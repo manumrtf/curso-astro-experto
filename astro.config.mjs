307d284282db4ebc6f7c8d1a6f79689413194020
manumrtf <manuelmartindev@outlook.com>
2024-12-02 17:00:25 +0100

    primera-clase

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind()]
+});
